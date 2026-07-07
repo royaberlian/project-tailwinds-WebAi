c5afdbf38dd01b5b20af860f5981bd2cd8c71c9c
royaberlian <royaberlian@gmail.com>
2026-07-08 02:41:38 +0330

    Add files via upload

---
+++ js/master.js
@@ -0,0 +1,15 @@
+// nav click
+const items = document.querySelectorAll(".menu>li")
+items.forEach(val => {
+    val.addEventListener("click", () => {
+        items.forEach(i => {
+            i.classList.remove("is-active")
+            val.classList.add("is-active")
+        })
+    })
+})
+// nav scroll
+const navbar = document.querySelector("#navbar")
+window.addEventListener("scroll", () => {
+    navbar.classList.toggle("navbar-sticky", window.scrollY > 20)
+})
