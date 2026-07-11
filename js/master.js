509efe2d921b7b563343e0a0fdf36e70b74a0128
royaberlian <royaberlian@gmail.com>
2026-07-11 07:07:23 +0330

    Add files via upload

--- js/master.js
+++ js/master.js
@@ -1,3 +1,9 @@
+AOS.init({
+  duration: 800,
+  once: false,
+});
+
+
 // nav click
 const items = document.querySelectorAll(".menu>li")
 items.forEach(val => {
@@ -14,3 +20,38 @@ const navbar = document.querySelector("#navbar")
 window.addEventListener("scroll", () => {
     navbar.classList.toggle("navbar-sticky", window.scrollY > 20)
 })
+
+
+
+// timer
+const timers = document.querySelectorAll(".countdown");
+
+let day = 3;
+let hour = 23;
+let minute = 38;
+let second = 40;
+
+setInterval(() => {
+    second--;
+
+    if (second < 0) {
+        second = 59;
+        minute--;
+    }
+
+    if (minute < 0) {
+        minute = 59;
+        hour--;
+    }
+
+    if (hour < 0) {
+        hour = 23;
+        day--;
+    }
+
+    timers.forEach(timer => {
+        timer.innerHTML = `${day} : ${hour} : ${minute} : ${second}`;
+    });
+
+}, 1000);
+
